906656e65a6ca0a481558ca0da84001cfa0af867
Mark <markh82@gmail.com>
2019-08-15 13:01:25 +0200

    Testing commit to dev branch

--- rm_discord_bot.js
+++ rm_discord_bot.js
@@ -137,6 +137,12 @@ client.on("message", async message => {
         message.channel.bulkDelete(fetched)
             .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
     }
+
+    if (command === "roulette") {
+        // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+        // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+        const m = await message.channel.send("Pulls trigger");
+    }
 });
 
 
