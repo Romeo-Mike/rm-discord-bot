5c7b3dd034730d3fd401d9e5d9f8f36112beadac
Craig Potter <craigp@iled.co.za>
2019-08-15 11:47:03 +0200

    Connects and lists channels of bot's connected servers.

--- rm_discord_bot.js
+++ rm_discord_bot.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-var sourceFile = require('./tokens_and_other_sensitive_crap.js');
-
-console.log(sourceFile.discord_bot_api_key);
+var important_stuff = require('./tokens_and_other_sensitive_crap.js');
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
+    // List servers the bot is connected to
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+        // List all channels
+        guild.channels.forEach((channel) => {
+            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+        })
+    })
 })
 
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
-bot_secret_token = "XXXXXXXXXXX"
 
-//client.login(bot_secret_token)
+
+client.login(important_stuff.discord_bot_api_key)
